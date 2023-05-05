bf5064c274d21cff0c088fb8de2bf370e0f1195e
斎藤正大 <masahiro@saitoumasahironoMacBook-Pro.local>
2023-05-05 17:33:23 +0900

    update migration file

--- api/migrate_db.py
+++ api/migrate_db.py
@@ -8,7 +8,7 @@ engine = create_engine(DB_URL, echo=True)
 
 def reset_database():
   Base.metadata.drop_all(bind=engine)
-  Base.metadata.create_dll(bind=engine)
+  Base.metadata.create_all(bind=engine)
 
 
 if __name__ == "__main__":
