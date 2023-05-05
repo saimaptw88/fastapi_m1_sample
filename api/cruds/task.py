5193290f57f7e6ab048117d358d192e4f92729f7
斎藤正大 <masahiro@saitoumasahironoMacBook-Pro.local>
2023-05-05 18:07:27 +0900

    add get

--- api/cruds/task.py
+++ api/cruds/task.py
@@ -1,7 +1,23 @@
+from typing import List, Tuple
+from sqlalchemy.engine import Result
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 
 import api.models.task as task_model
 import api.schemas.task as task_schema
+
+
+async def get_tasks_with_done(db: AsyncSession):
+  result: Result = await (
+    db.execute(
+      select (
+        task_model.Task.id,
+        task_model.Task.title,
+        task_model.Done.id.isnot(None).label("done"),
+      ).outerjoin(task_model.Done)
+    )
+  )
+  return result.all()
 
 
 async def create_task(
--- api/routers/task.py
+++ api/routers/task.py
@@ -10,8 +10,8 @@ import api.schemas.task as task_schema
 router = APIRouter()
 
 @router.get("/tasks", response_model=List[task_schema.Task])
-async def list_tasks():
-  return [task_schema.Task(id=1, title="1つ目のTODOタスク")]
+async def list_tasks(db: AsyncSession=Depends(get_db)):
+  return await task_crud.get_tasks_with_done(db)
 
 
 @router.post("/tasks", response_model=task_schema.TaskCreateResponse)
